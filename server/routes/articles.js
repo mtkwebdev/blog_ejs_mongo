0ce4304d1c026491da67a21d2a02a382b9efee12
mtkwebdev <mtkdev1.0@gmail.com>
2022-07-17 23:24:20 +0100

    adding article routes and mongoose

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('List of articles')
+const Article = require('../Models/articles')
+
+// router.get('/:id',(req,res){})
+
+router.post('/', async(req, res) => {
+    // res.send('List of articles')
+    const article = new Article({
+      title: req.body.title,
+      description: req.body.description,
+      markdown:req.body.markdown,
+    })
+    try{
+      const newArticle = await article.save()
+      res.redirect(`/articles/${article.id}`, res.status = 200)
+    }catch(err){
+      res.redirect('/articles/new', {article: article})
+      throw err
+    }
   })
-router.get('/post', (req, res) => {
-    res.send('a single article')
+
+router.get('/new', (req, res) => {
+    res.render('../views/articles/new')
   })
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const mongoose = require('mongoose')
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+// app.use(express.json());
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/blog')
 
